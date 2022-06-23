d94bb1a6d6792ef97ab821fc3a1b0a524b9bc03c
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-06-23 22:20:33 +0600

    make nested route

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function profile() {
+  return (
+    <div>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <h1>Your Profile</h1>
+    </div>
+  );
+}
